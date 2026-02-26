85c4d10a59ac142e306dde6163498668de3a2619
Robert Shaw <rshaw@neuralmagic.com>
2026-02-26 17:17:43 -0500

    updated
    Signed-off-by: Robert Shaw <rshaw@neuralmagic.com>

--- guides/wide-ep-lws/run-bench.sh
+++ guides/wide-ep-lws/run-bench.sh
@@ -5,8 +5,8 @@
 # ==============================================================================
 NAMESPACE="${NAMESPACE:-llm-d-nebius}"
 GATEWAY_NAME="${GATEWAY_NAME:-llm-d-inference-gateway}"
-BENCHMARK_DIR="${BENCHMARK_DIR:-./bench-dp-ep-multi-node-test}"
-OUTPUT_DIR="${OUTPUT_DIR:-./bench-dp-ep-multi-node-test-results}"
+BENCHMARK_DIR="${BENCHMARK_DIR:-./long-context}"
+OUTPUT_DIR="${OUTPUT_DIR:-./long-context-wide-ep}"
 # RAW_IP="${RAW_IP:-10.145.217.87}"
 # RAW_PORT="${RAW_PORT:-80}"
 
